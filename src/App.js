dcf96bb76c0f6332de4bde0c05b17ccd09f8e545
Abood <dev7.abood@gmail.com>
2022-07-30 00:43:05 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,175 @@
+import './App.css';
+import testSvg from './testSvg.svg'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+    return (
+        <>
+            <div className="position-relative">
+                <div className="position-absolute">
+                    <div className="text-center d-flex">
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(11)}
+                             style={{height: '270px', width: '74px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{top: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>11</div>
+                        </div>
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(12)}
+                             style={{height: '270px', width: '61px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{top: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>12</div>
+                        </div>
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(13)}
+                             style={{height: '270px', width: '61px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{top: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>13</div>
+
+                        </div>
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(14)}
+                             style={{height: '270px', width: '50px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{top: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>14</div>
+
+                        </div>
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(15)}
+                             style={{height: '270px', width: '43px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{top: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>15</div>
+
+                        </div>
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(16)}
+                             style={{height: '270px', width: '41px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{top: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>16</div>
+
+                        </div>
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(17)}
+                             style={{height: '270px', width: '38px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{top: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>17</div>
+
+                        </div>
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(18)}
+                             style={{height: '270px', width: '58px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{top: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>18</div>
+
+                        </div>
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(21)}
+                             style={{height: '270px', width: '58px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{top: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>21</div>
+
+                        </div>
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(22)}
+                             style={{height: '270px', width: '39px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{top: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>22</div>
+
+                        </div>
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(23)}
+                             style={{height: '270px', width: '32px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{top: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>23</div>
+
+                        </div>
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(24)}
+                             style={{height: '270px', width: '48px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{top: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>24</div>
+
+                        </div>
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(25)}
+                             style={{height: '270px', width: '50px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{top: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>25</div>
+
+                        </div>
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(26)}
+                             style={{height: '270px', width: '63px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{top: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>26</div>
+
+                        </div>
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(27)}
+                             style={{height: '270px', width: '58px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{top: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>27</div>
+                        </div>
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(28)}
+                             style={{height: '270px', width: '58px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{top: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>16</div>
+
+                        </div>
+                    </div>
+                    <div className="text-center d-flex">
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(11)}
+                             style={{height: '270px', width: '74px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{bottom: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>11</div>
+                        </div>
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(12)}
+                             style={{height: '270px', width: '61px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{bottom: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>12</div>
+                        </div>
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(13)}
+                             style={{height: '270px', width: '61px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{bottom: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>13</div>
+
+                        </div>
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(14)}
+                             style={{height: '270px', width: '50px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{bottom: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>14</div>
+
+                        </div>
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(15)}
+                             style={{height: '270px', width: '43px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{bottom: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>15</div>
+
+                        </div>
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(16)}
+                             style={{height: '270px', width: '41px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{bottom: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>16</div>
+
+                        </div>
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(17)}
+                             style={{height: '270px', width: '38px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{bottom: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>17</div>
+
+                        </div>
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(18)}
+                             style={{height: '270px', width: '58px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{bottom: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>18</div>
+
+                        </div>
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(21)}
+                             style={{height: '270px', width: '58px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{bottom: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>21</div>
+
+                        </div>
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(22)}
+                             style={{height: '270px', width: '39px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{bottom: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>22</div>
+
+                        </div>
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(23)}
+                             style={{height: '270px', width: '32px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{bottom: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>23</div>
+
+                        </div>
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(24)}
+                             style={{height: '270px', width: '48px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{bottom: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>24</div>
+
+                        </div>
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(25)}
+                             style={{height: '270px', width: '50px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{bottom: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>25</div>
+
+                        </div>
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(26)}
+                             style={{height: '270px', width: '63px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{bottom: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>26</div>
+
+                        </div>
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(27)}
+                             style={{height: '270px', width: '58px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{bottom: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>27</div>
+                        </div>
+                        <div className="mx-1 position-relative" role="button" onClick={_ => console.log(28)}
+                             style={{height: '270px', width: '58px', backgroundColor: 'rgba(0, 0, 0, 0.3)'}}>
+                            <div className='position-absolute text-center' style={{bottom: '15px', left : '50%', transform : 'translate(-50%, 0)'}}>16</div>
+
+                        </div>
+                    </div>
+                </div>
+                <img src={testSvg} width="1000" alt=""/>
+            </div>
+        </>
+    );
+}
+
+export default App;
